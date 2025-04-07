fcbe7ee86bdaf9587d57bbfb0338560fc2488871
prabhanjansp <puranikchetan97@gmail.com>
2025-04-08 00:23:04 +0530

    env space update

--- src/services/newsAPI.js
+++ src/services/newsAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "deebd35b1bb94d59935f55d62eb2c674"; 
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY; // Make sure to set this in your .env file 
 const BASE_URL = "https://newsapi.org/v2/";
 
 export const fetchCryptoNews = async (page = 1, pageSize = 6) => {
